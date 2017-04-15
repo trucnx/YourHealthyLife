e94756cdf2593b5f111ba955c0db8c3b5776d46d
trucnx <chukku410@aol.com>
2017-04-15 12:57:59 +1000

    01

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
+project = u'Healthy and Happy Lifestyle'
 copyright = u'2017, YourHealthyLife - Ms.Minh'
 
 # The version info for the project you're documenting, acts as replacement for
